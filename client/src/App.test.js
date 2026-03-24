9aa8294e7a8b5f40089ffeff20b72f09ee201ed5
krish9724 <149668631+krish9724@users.noreply.github.com>
2026-03-24 18:50:26 +0530

    Delete client/src/App.test.js

--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders app heading', () => {
-  render(<App />);
-  const heading = screen.getByText(/CI\/CD Task Manager/i);
-  expect(heading).toBeInTheDocument();
-});
